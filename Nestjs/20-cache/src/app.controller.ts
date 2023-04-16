913defd9f44beda6b29a74e08ef08f412673820d
Dina Berry <dinaberry@outlook.com>
2023-04-16 16:50:47 -0700

    ability to see cache

--- Nestjs/20-cache/src/app.controller.ts
+++ Nestjs/20-cache/src/app.controller.ts
@@ -1,11 +1,27 @@
 import { CacheInterceptor } from '@nestjs/cache-manager';
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Body, Post, UseInterceptors } from '@nestjs/common';
+import { IsInt, IsString } from 'class-validator';
 
-@Controller()
+class CreateItemDto {
+  @IsInt()
+  readonly id: number;
+
+  @IsString()
+  readonly name: string;
+}
+
+@Controller('items')
 @UseInterceptors(CacheInterceptor)
 export class AppController {
+
+  data = [{ id: 1, name: 'Nest' }]
+
   @Get()
   findAll() {
-    return [{ id: 1, name: 'Nest' }];
+    return this.data;
+  }
+  @Post()
+  async create(@Body() createItemDto: CreateItemDto) {
+    this.data.push(createItemDto);
   }
 }
--- Nestjs/20-cache/src/app.module.ts
+++ Nestjs/20-cache/src/app.module.ts
@@ -1,9 +1,10 @@
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AdminController } from './cache.controller';
 
 @Module({
   imports: [CacheModule.register()],
-  controllers: [AppController],
+  controllers: [AppController, AdminController],
 })
 export class AppModule {}
